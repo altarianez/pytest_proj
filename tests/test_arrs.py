0fe9013952ac810276d5cdda697eb211266f5d83
Aleksey Borodin <altarian87@mail.ru>
2023-04-05 02:41:32 +0500

    add parametrize to test_get

--- tests/test_arrs.py
+++ tests/test_arrs.py
@@ -1,16 +1,18 @@
-from utils import arrs
+from utils.arrs import get, my_slice
+import pytest
 
 
-def test_get():
-    assert arrs.get([1, 2, 3], 1, "test") == 2
-    assert arrs.get([], 0, "test") == "test"
-    assert arrs.get([], -1, "test") == "test"
+@pytest.mark.parametrize("a, b, default", [([1, 2, 3], 1, 2),
+                                          ([], 0, None),
+                                          ([1, 2, 3], -1, None)])
+def test_get(a, b, default):
+    assert get(a, b) == default
 
 
 def test_slice():
-    assert arrs.my_slice([1, 2, 3, 4], 1, 3) == [2, 3]
-    assert arrs.my_slice([1, 2, 3, 4], -2) == [3, 4]
-    assert arrs.my_slice([1, 2, 3, 4], -5) == [1, 2, 3, 4]
-    assert arrs.my_slice([1, 2, 3, 4], 0, 2) == [1, 2]
-    assert arrs.my_slice([1, 2, 3], 1) == [2, 3]
-    assert arrs.my_slice([], 1) == []
+    assert my_slice([1, 2, 3, 4], 1, 3) == [2, 3]
+    assert my_slice([1, 2, 3, 4], -2) == [3, 4]
+    assert my_slice([1, 2, 3, 4], -5) == [1, 2, 3, 4]
+    assert my_slice([1, 2, 3, 4], 0, 2) == [1, 2]
+    assert my_slice([1, 2, 3], 1) == [2, 3]
+    assert my_slice([], 1) == []
